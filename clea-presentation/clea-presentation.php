ac7a5b6efd8a8c6441fb2f1f3972820cfa8afb44
Anne-Laure Delpech <contact@parcours-performance.com>
2016-08-20 08:46:33 +0200

    2 sous-menus testés

--- clea-presentation/clea-presentation.php
+++ clea-presentation/clea-presentation.php
@@ -48,6 +48,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	
 	// générer une page de réglages de l'extension pour l'administrateur
 	require_once CLEA_PRES_DIR_PATH . 'admin/clea-presentation-settings-page.php';
+	require_once CLEA_PRES_DIR_PATH . 'admin/clea-presentation-settings-page-2.php';
 
 
 
